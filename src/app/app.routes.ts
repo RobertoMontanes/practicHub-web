39ef8ed19c3e350f1fbfda12e46339103cc232c3
Roberto <montanes.rarob24@triana.salesianos.edu>
2025-11-06 10:40:49 +0100

    Todas las páginas funcionan a nivel de relaciones con la barra de búsqueda

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,9 @@ import { LoginPages } from './pages/login-pages/login-pages';
 import { SingUpPages } from './pages/sing-up-pages/sing-up-pages';
 import { DashboardPages } from './pages/dashboard-pages/dashboard-pages';
 import { EmpresasPages } from './pages/empresas-pages/empresas-pages';
+import { AlumnosPages } from './pages/alumnos-pages/alumnos-pages';
+import { FirmaDigitalPages } from './pages/firma-digital-pages/firma-digital-pages';
+import { ProfesoresPages } from './pages/profesores-pages/profesores-pages';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
@@ -10,6 +13,8 @@ export const routes: Routes = [
   { path: 'registro', component: SingUpPages },
   { path: 'dashboard', component: DashboardPages },
   { path: 'empresas', component: EmpresasPages },
+  { path: 'alumnos', component: AlumnosPages },
+  {path: 'firma-digital', component: FirmaDigitalPages},
+  { path: 'profesores', component: ProfesoresPages},
   { path: '**', redirectTo: '/login' }
-  
 ];
--- src/app/pages/empresas-pages/empresas-pages.ts
+++ src/app/pages/empresas-pages/empresas-pages.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-empresas-pages',
-  imports: [],
+  standalone: true,  
+  imports: [RouterModule],
   templateUrl: './empresas-pages.html',
   styleUrl: './empresas-pages.css',
 })
