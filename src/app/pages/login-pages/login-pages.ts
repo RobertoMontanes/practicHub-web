55fe6ae5906283d995b00bef8231cea189ea4fb2
Roberto <rmr15092004@gmail.com>
2025-11-14 08:27:27 +0100

    Actualización vistas

--- src/app/pages/login-pages/login-pages.ts
+++ src/app/pages/login-pages/login-pages.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators, AbstractControl } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login-pages',
   standalone: true,  
-  imports: [RouterModule],  
+  imports: [RouterModule, ReactiveFormsModule],  
   templateUrl: './login-pages.html',
   styleUrls: ['./login-pages.css']
 })
 export class LoginPages {
+  loginForm = new FormGroup({
+    emailFormControl: new FormControl('', [Validators.required, Validators.email]),
+    passwordFormControl: new FormControl('', [Validators.required, Validators.minLength(6)])
+  })
 }
